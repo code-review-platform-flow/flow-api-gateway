90601699f4ef1b364ccc2eeff92b003c7c75ee29
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-07-19 21:06:20 +0900

    first commit

---
+++ src/main/java/org/example/flowmain/FlowMainApplication.java
@@ -0,0 +1,13 @@
+package org.example.flowmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/flowmain/FlowMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flowmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
