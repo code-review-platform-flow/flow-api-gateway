84013188c0c169f7a7b262906ea31e9588de6bd0
gypark <a1061602@gmail.com>
2024-08-06 09:02:04 +0900

    fix: 코드 정리

--- src/main/java/org/flow/gateway/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package org.flow.gateway.repository;
-
-import org.flow.gateway.entity.UsersEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface UsersRepository extends ReactiveCrudRepository<UsersEntity, Long> {
-    Mono<UsersEntity> findByEmail(String email);
-}
--- src/main/java/org/flow/gateway/service/usersessions/persistence/UserSessionsService.java
+++ src/main/java/org/flow/gateway/service/usersessions/persistence/UserSessionsService.java
@@ -1,12 +1,8 @@
 package org.flow.gateway.service.usersessions.persistence;
 
-import org.flow.gateway.dto.usersessions.UserSessionsDto;
 import org.flow.gateway.entity.UserSessionsEntity;
-import org.flow.gateway.mapper.UserSessionsMapper;
-import org.flow.gateway.mapper.UsersMapper;
 import org.flow.gateway.repository.UserSessionsRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.reactive.TransactionalOperator;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -16,32 +12,10 @@ import reactor.core.publisher.Mono;
 public class UserSessionsService {
 
 	private final UserSessionsRepository userSessionsRepository;
-	private final UserSessionsMapper userSessionsMapper;
-	private final UsersMapper usersMapper;
-	private final TransactionalOperator transactionalOperator;
-
-	public Mono<UserSessionsDto> save(UserSessionsDto userSessionsDto) {
-		UserSessionsEntity userSessionsEntity = userSessionsMapper.toEntity(userSessionsDto);
-		return userSessionsRepository.save(userSessionsEntity)
-			.map(userSessionsMapper::toDto)
-			.as(transactionalOperator::transactional);
-	}
 
 	public Mono<UserSessionsEntity> getSessionByAccessToken(String accessToken) {
 		return userSessionsRepository.findByAccessToken(accessToken)
-			.filter(UserSessionsEntity::isUseYn);  // useYn 필터링 추가
+			.filter(UserSessionsEntity::isUseYn);
 	}
-
-	// public UserSessionsDto findByUserId(UsersDto usersDto){
-	//     UsersEntity usersEntity = usersMapper.toEntity(usersDto);
-	//     UserSessionsEntity userSessionsEntity = userSessionsRepository.findByUserId(usersEntity.getUserId())
-	//         .orElseThrow(() -> new EntityNotFoundException("UserSession not found with user"));
-	//     return userSessionsMapper.toDto(userSessionsEntity);
-	// }
-	//
-	// public UserSessionsDto existsByUserId(UsersDto usersDto){
-	//     Optional<UserSessionsEntity> userSessionsEntity = userSessionsRepository.findByUserId(usersDto.getUserId());
-	//     return userSessionsEntity.map(userSessionsMapper::toDto).orElse(UserSessionsDto.builder().build());
-	// }
 
 }
