7cb90a42cb6c571025808177c9ad67d956fb42bf
gypark <a1061602@gmail.com>
2024-08-14 15:44:10 +0900

    feat: gateway 설정

--- src/main/java/org/flow/gateway/common/security/SecurityConfig.java
+++ src/main/java/org/flow/gateway/common/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,13 +14,10 @@ public class SecurityConfig {
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		http
-			.csrf(csrf -> csrf
-				.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()) // CSRF 토큰을 쿠키로 전송
-				.disable() // 개발 또는 특정 경로에 대해 CSRF 보호 비활성화
+			.csrf(ServerHttpSecurity.CsrfSpec::disable
 			)
 			.authorizeExchange(exchanges -> exchanges
-				.pathMatchers("/auth/**", "/api/admin/**").permitAll() // /auth/** 경로는 인증 없이 접근 가능
-				.anyExchange().authenticated() // 다른 모든 경로는 인증 필요
+				.anyExchange().permitAll()
 			)
 			.formLogin(Customizer.withDefaults())
 			.httpBasic(Customizer.withDefaults());
