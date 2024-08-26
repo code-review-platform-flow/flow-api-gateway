0a7a34cc8f04258ecb84dbc38ec80f5653a72e92
gypark <a1061602@gmail.com>
2024-08-26 10:51:41 +0900

    feat: order 수정

--- src/main/java/org/flow/gateway/common/security/SecurityConfig.java
+++ src/main/java/org/flow/gateway/common/security/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,7 +15,6 @@ public class SecurityConfig {
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		http
 			.csrf(ServerHttpSecurity.CsrfSpec::disable)
-			.cors(cors -> cors.configurationSource(corsConfigurationSource()))  // CORS 설정 추가
 			.authorizeExchange(exchanges -> exchanges
 				.anyExchange().permitAll()
 			)
@@ -27,16 +24,4 @@ public class SecurityConfig {
 		return http.build();
 	}
 
-	@Bean
-	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOrigin("http://localhost:3000");
-		configuration.addAllowedMethod("*");
-		configuration.addAllowedHeader("*");
-		configuration.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
 }
